866f9e384d5f544c5114da2bab59f6a7fd73a00c
Jacob Asper <jacobasper191@gmail.com>
2023-09-28 21:56:09 -0400

    add boilerplate to login as discord bot

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import job from './cron.js';
 import 'dotenv/config';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 
-job.start();
+// job.start();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, (c) => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
